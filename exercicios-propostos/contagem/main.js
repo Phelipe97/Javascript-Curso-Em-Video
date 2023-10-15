a11daa1199f07ef20bff3e9a41aec1145fc5b9a8
Phelipe Santos <phe2111@hotmail.com>
2023-10-15 10:41:11 -0300

    adicionando mais liçoes

---
+++ exercicios-propostos/contagem/main.js
@@ -0,0 +1,12 @@
+var inicio = document.getElementById('inicio')
+    var fim = document.getElementById('fim')
+    var passo = document.getElementById('passo')
+    var res = document.getElementById('res')
+    
+function contar() {
+    if (passo.value==0) {
+        alert('passo zerado preenchao e tente novamente')
+    } else {
+        
+    }
+}
