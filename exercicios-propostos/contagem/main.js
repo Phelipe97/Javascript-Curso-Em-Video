6169e0e943accce72a91eb54174a66884d64a507
Phelipe Santos <phe2111@hotmail.com>
2023-10-17 21:30:24 -0300

    Contagem completo

--- exercicios-propostos/contagem/main.js
+++ exercicios-propostos/contagem/main.js
@@ -1,12 +1,36 @@
-var inicio = document.getElementById('inicio')
-    var fim = document.getElementById('fim')
-    var passo = document.getElementById('passo')
     var res = document.getElementById('res')
     
 function contar() {
-    if (passo.value==0) {
-        alert('passo zerado preenchao e tente novamente')
+    let inicio = document.getElementById('inicio')
+    let fim = document.getElementById('fim')
+    let passo = document.getElementById('passo')
+
+    if (inicio.value.length == 0 || fim.value.length == 0 || passo.value.length == 0) {
+        res.innerHTML = 'Impossivel contar faltam dados...'
+        //alert('[Erro] faltam dados')
     } else {
-        
+        res.innerHTML = 'Contando: <br>'
+        let i = Number(inicio.value)
+        let f = Number(fim.value)
+        let p = Number(passo.value)
+
+        //Caso digite passo 0 ou numero negativo
+        if (p <= 0) {
+            alert('[Erro] passo invalido! Considerando PASSO 1')
+            p = 1
+        }
+
+        if (i < f) {
+            //Contagem Crescente
+            for (let c = i; c <= f; c += p) {
+                res.innerHTML += ` ${c} \u{1F449} `
+            }
+        } else {
+            //Contagem Decrecente
+            for (let c = i; c >= f; c -= p) {
+                res.innerHTML += ` ${c} \u{1F449} `
+            }
+        }
+        res.innerHTML += ` \u{1F3C1} `
     }
 }
